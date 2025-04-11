d8183c22a18ff67c0e46f752e1b72f38c30465ad
Eang Mengkong <eangmengkong21@gmail.com>
2025-04-11 09:59:43 +0700

    edit addbtn

--- src/components/ButtonAddHabits.tsx
+++ src/components/ButtonAddHabits.tsx
@@ -4,10 +4,11 @@ interface ButtonAddHabitsProps {
   onClick?: () => void;
 }
 
-const ButtonAddHabits: React.FC<ButtonAddHabitsProps> = () => {
+const ButtonAddHabits: React.FC<ButtonAddHabitsProps> = ({ onClick }) => {
   return (
     <button
       type="submit"
+      onClick={onClick}
       className="px-6 py-3 bg-purple-600 hover:bg-purple-700 rounded-lg font-semibold transition-colors duration-200 text-white"
       aria-label="Add new habit"
     >
